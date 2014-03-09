0d14bdb745681bfb76aa7e509e062bf7ea53fe8e
Joe Fiorini <joe@joefiorini.com>
2014-03-09 19:34:36 -0400

    Add route for posts without comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 AmsBlog::Application.routes.draw do
 
   resources :posts do
-    get "/no_comments", to: "posts#no_comments"
+    collection do
+      get "/no_comments", to: "posts#no_comments"
+    end
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
