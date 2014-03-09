9075ca98bfe830fe5dd2bf8e31267d9e61c272ce
Joe Fiorini <joe@joefiorini.com>
2014-03-09 18:57:09 -0400

    Initial setup with models

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AmsBlog::Application.config.secret_key_base = 'f76c85473d3a8dd9e1664049c23aa23ffd957ad55bc46deff6fd0d0b9a3ae4eb97062a779c2bf00cc1cd3dabecfb4954007902522c92635e3a69be6777b08377'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AmsBlog::Application.config.session_store :cookie_store, key: '_ams_blog_session'
